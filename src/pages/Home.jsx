11a01ad30d5eb484a2c6fbc1eadbad88633167b6
Nikita <kushoyvibecode@gmail.com>
2026-09-19 15:42:20 +0300

    Add basic React Router navigation

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+
+function Home() {
+    return (
+        <section>
+            <h1>Главная страница</h1>
+
+            <p>
+                Учебный проект для изучения React Router
+            </p>
+        </section>
+    );
+}
+export default Home;
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,14 @@
+
+function Projects() {
+    return (
+        <section>
+            <h1>Проекты</h1>
+
+            <p>
+                Здесь позже появится список проектов
+            </p>
+        </section>
+    );
+}
+
+export default Projects;
